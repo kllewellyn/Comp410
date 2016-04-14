88414bfaceef60f67a180e36e40db694403831bc
Your Name <you@example.com>
2016-04-14 18:16:35 -0400

    Google Code Jam

---
+++ Assignment06/src/assn6/AssnTopoSort.java
@@ -0,0 +1,24 @@
+package assn6;
+
+public class AssnTopoSort {
+
+	  public static void main (String[] args) {
+	    // your code to create and exercise a graph adt
+	    // and its topo sort operation
+
+	    // thorough testing is your responsibility
+
+	    // you may wish to create an interactive driver
+	    // like we did for BST/SPLT, but this is not required 
+	    //
+	    // you may wish to create methods like 
+	    //    -- print
+	    //    -- sort
+	    //    -- random fill
+	    //    -- etc.
+	    // in order to convince yourself your code is producing
+	    // the correct behavior
+	  }
+
+	  // anything else you need to add in
+	}
---
+++ Assignment06/src/assn6/Edge.java
@@ -0,0 +1,15 @@
+package assn6;
+
+public class Edge {
+	
+	Node front, back;
+	long weight;
+	
+	public Edge(long w, Node f, Node b){
+		weight = w;
+		front = f;
+		back = b;
+		
+	}
+
+}
